81487dff455a1d536dbdee3327a779e227464c64
Aman gupta <amang4996@gmail.com>
2022-01-13 22:51:55 +0530

    Update Queries_practice.sql

--- Queries_practice.sql
+++ Queries_practice.sql
@@ -1,4 +1,7 @@
--- from geeks for geeks sql (youtube)
+**********************************
+---Geeks for Geeks sql (youtube)
+**********************************
+
 --second heighest salary of an employee
 select * from(
 select *,
@@ -100,9 +103,9 @@ and e1.employee_id <> e2.employee_id;
 
 
 
---------------------------------------
---Master SQL for DS Course(BRAVE PART)
----------------------------------------
+********************************************
+--Master SQL for DS Udemy Course(BRAVE PART)
+********************************************
 use job_ready;
 
 select * from students;
@@ -134,8 +137,11 @@ select s1.* from students s1
 inner join student_enrollment s2
 on s1.student_no = s2.student_no
 where course_no <> 'CS220'
----------------------------------------------------------------------
+
+
+-------------------------
 use wikibooks_sql;
+-------------------------
 
 --3. Select the name of the products with a price less than or equal to $200.
 select name,Price
@@ -200,9 +206,10 @@ insert into Products(code,name,price,Manufacturer) values(11,'Loudspeakers',70,2
 update Products set name = 'Laser Printer' where code = 8;
 
 
--------------------------------
--- ItJunction4all youtube sql 
--------------------------------
+--------------------------------------------------
+-- SQL Scenario Based question (ITJunctionfor All)
+--------------------------------------------------
+
 use dataset;
 
 --1
@@ -234,7 +241,6 @@ group by [group],[split]
 order by [group];
 
 --3
-
 --Student Table has three columns Student_Name, Total_Marks and Year.
 --User has to write a SQL query to display Student_Name, Total_Marks, Year,  Prev_Yr_Marks 
 --for those whose Total_Marks are greater than or equal to the previous year.
@@ -321,7 +327,6 @@ dense_rank()over(partition by accountnumber order by transactiontime desc) as rn
 from Transaction_Table) temp
 where temp.rn = 1
 
-
 --14 cumulative sum 
 select *,
 sum(quantity)over(partition by productcode order by inventorydate) as running_total
@@ -386,17 +391,305 @@ max(dates) as end_date
 from temp2
 group by balance,total
 
---23
+
+--23(not solved)
 select distinct
 case when start_location > end_location  then end_location else start_location end as source,
 case when start_location > end_location  then start_location else end_location end as dest,
 distance
-from dbo.Travel_Table
+from dbo.Travel_Table;
 
 Select Start_Location,End_Location,Distance from (
 Select Start_Location,End_Location,Distance,row_number() over(Partition by Distance order by Distance) as Row_Num 
-from Travel_Table) A where Row_Num = 1
+from Travel_Table) A where Row_Num = 1;
 
+-- 22/12/21
+-- scenario based questions(youtube)
+--38
+
+with t1 as (
+select  *,
+lead(id,1)over(order by id) as la,
+lead(id,2)over(order by id) as la2
+from stadium a
+where a.No_Of_People >= 100),
+t2 as (
+select * from t1 
+left join Stadium s
+on s.id = t1.id or s.id = t1.la or s.id = t1.la2
+);
+
+--2 approach
+with temp as (
+select *, RANK()over(order by id) as rn
+from stadium 
+where No_Of_People>=100)
+
+select id,Visit_Date,No_Of_People from temp
+where id-rn > 1
+
+
+--37
+with temp as (
+select *,
+sum(sales)over(partition by continents, country) as total
+from salesinfo),
+t2 as (
+select continents, country, total,
+dense_rank()over(partition by continents order by total desc) as dk
+from temp)
+
+select distinct continents, country, total from t2
+where dk = 1
+
+--23/12/11
+
+--36 adjcent seat
+
+select * from SeatArrangement;
+
+select 
+    case when id % 2 <> 0 and id = (select count(*) from SeatArrangement) then id
+         when id % 2 <> 0 then id+1
+         when id % 2 = 0  then id - 1
+    end as id,
+    studentname 
+from SeatArrangement
+
+
+--35
+--1
+select * from employees
+where binary_checksum(upper(first_name)) <> binary_checksum(first_name) 
+
+--OR
+
+select * from employees 
+where upper(First_name) !=  First_name COLLATE SQL_Latin1_General_CP1_CS_AS
+
+--2
+select * from employees
+
+select 
+SUBSTRING(First_name,1, CHARINDEX(' ', First_name)-1) as fname,
+substring(First_name,CHARINDEX(' ', First_name)+1, LEN(First_name)) as lname
+from Employees
+where last_name is null or last_name  = ' ';
+
+update Employees
+set First_name = SUBSTRING(First_name,1, CHARINDEX(' ', First_name)-1),
+ last_name = substring(First_name,CHARINDEX(' ', First_name), LEN(First_name))
+where last_name is null or last_name  = ' ';
+
+
+--3
+declare @ex as date set @ex =  '2017-06-30';
+with temp as (
+select *,datediff(year,Joining_date,@ex) as counts from employees),
+
+temp2 as(
+    select 
+    case when counts < 1 then '< 1 years'
+          when counts >= 1 and counts < 3 then '1-3 years' 
+          when counts >=3 and counts < 5 then '3-5 years'
+          when counts >=5 then '5+ years'
+    end as tenure_in_years
+from temp
+)
+
+select tenure_in_years, count(*) as employee_counts from temp2
+group by tenure_in_years
+
+--4
+select * from employees e
+where day(e.Birth_date) = day(e.Joining_date)
+
+--5
+declare @ex as date set @ex =  '2017-06-30';
+with temp as (
+select *,datediff(year,Joining_date,@ex) as counts from employees
+)
+select * from temp
+where temp.counts> 5 and year(Birth_date) = (select max(year(birth_date)) from Employees)
+
+--34
+with temp as (
+select distinct quote_id, Order_Status from OrderStatus
+),
+temp2 as(
+select quote_id, STRING_AGG(order_status,',') as list from temp
+group by quote_id
+)
+select Quote_id,
+    case when CHARINDEX('Delivered',list) = 1 and CHARINDEX(',',list) = 0  then 'Completed'
+         when CHARINDEX('Delivered',list) > 1 and CHARINDEX(',',list) >0 then 'In Delivery'
+         when CHARINDEX('Submitted',list) > 1 and CHARINDEX(',',list) >0 and CHARINDEX('Delivered',list) = 0 then 'Awaiting for Submission'
+         else 'Awaiting for Entry'
+    end as ord_status
+from temp2;
+
+--26/12/21
+--33
+--Pattern question(not able to understand)
+
+--Base Query
+with ap as(
+select 'INTERVIEW' AS a, len('INTERVIEW') as b
+union all
+select substring(a,1,b-1), b-1
+from ap where b-1>0
+
+)
+select a from ap
+
+--32
+with temp as (
+select *,
+dense_rank()over(partition by deptno order by salary desc) as max_rk,
+dense_rank()over(partition by deptno order by salary asc) as min_rk
+from Employee_2
+)
+select empname, deptname, deptno, salary
+from temp
+where temp.max_rk = 1
+union all  
+select empname, deptname, deptno, salary
+from temp
+where temp.min_rk = 1
+order by temp.deptno, salary asc
+
+--31
+select * from StudentInfo_1
+
+select StudentName, English, Maths, Science
+from 
+(select StudentName, Subjects, Marks from StudentInfo_1 ) source_table 
+PIVOT
+(
+    max(marks) for subjects in (English, Maths, Science)
+) as pivot_table
+
+
+--30
+select * from sales1
+
+--1
+select *,
+sum(sales)over(partition by product order by sales) as total
+from sales1
+
+--2
+select *,
+min(sales)over(partition by product order by sales) as total
+from sales1
+
+--29
+--1
+select e1.employeename as emp_name, e2.employeename as man_name
+from employee_1 as e1, Employee_1 as e2
+where e1.managerid = e2.employeeid
+
+--2
+select e1.employeename as emp_name, ISNULL(e2.employeename, 'Boss') as man_name
+from employee_1 as e1
+left join Employee_1 as e2
+on e1.managerid = e2.employeeid
+ 
+ 
+ --27/12/21
+ --questions by Vishal Kaushal 
+
+--1
+select * from Employee
+
+select max(salary) from Employee
+where salary < (select max(salary) from employee)
+
+--2
+select * from retail
+
+select retailers, oneplus, realme, celkon, mi
+from (select retailers, brand from Retail) r 
+PIVOT
+(
+    max(retailers) for brand in (oneplus, realme, celkon, mi  ) 
+) as b
+
+
+--3
+select cast(10 as float)/cast(4 as float);
+select 1|1;
+Select 2|4;
+select 10|20
+select 40^30
+select ~20
+
+--4
+select 
+    count(case when ratio = 'MALE' then 1 else null end)*100/count(ratio) as 'Male%',
+    count(case when ratio = 'FEMALE' then 1 else null end)*100/count(ratio) as 'FEMALE%'
+from gender
+
+--28/12/21
+
+--28
+select x,y,z from (
+select *,
+count(*)over(partition by x,y) as dk
+from Sample_1
+)temp
+where temp.dk>1
+
+
+--29/12/21
+--18
+with temp1 as(
+select id from alpha
+),
+temp2 as(
+select b.id as id from beta b
+left join temp1 t
+on t.id = b.id
+where b.id not in (select id from temp1)
+)
+select id from temp2
+union all
+select id from temp1
+
+--19
+with temp1 as(
+select id,name,
+case when gender ='Male' then 'Female' end as "Gender"
+from example18
+where gender = 'Male'),
+temp2 as(
+select id,name,
+case when gender ='Female' then 'Male' end as "Gender"
+from example18
+where gender = 'Female')
+
+select id, name, gender from temp1
+union 
+select id, name, gender from temp2
+
+--simple apprach
+select id, name,
+case when gender = 'Male' then 'Female'
+     when gender = 'Female' then 'Male'
+end as "Gender"
+from example18
+
+--22
+with temp as (
+select  a.id as id, a.name as name, a.salary as salary, a.managerid as managerid
+from example22 a, example22 b
+where a.managerid = b.id
+),
+temp2 as (
+select *, avg(salary)over(PARTITION by managerid) as rn from temp
+)
+
+select managerid, name, rn from temp2  
 
 --40 
 with temp as (
@@ -455,5 +748,4 @@ select upper(left(isnull(app,'Offline'),1))+lower(SUBSTRING(isnull(app,'Offline'
 		dk2 as count from temp2
 where dk2<=2
 order by dk2 desc
-
 
