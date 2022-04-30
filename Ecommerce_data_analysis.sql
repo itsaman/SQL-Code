831177609b89b3acdeca59d81e65b76dd338e8fa
Aman gupta <amang4996@gmail.com>
2022-04-30 11:52:28 +0530

    Update Ecommerce_data_analysis.sql

--- Ecommerce_data_analysis.sql
+++ Ecommerce_data_analysis.sql
@@ -30,7 +30,40 @@ from website_sessions ws
 		on ord.website_session_id = ws.website_session_id
 where utm_source = 'gsearch' and utm_campaign = 'nonbrand' and ws.created_at < '2012-04-14';
 
--- 
+-- Traffic source trending
+select week_start_date, sessions from(
+select 	week(created_at), 
+		min(date(created_at)) as week_start_date, 
+		count(distinct website_session_id) as sessions 
+from website_sessions
+where utm_source = 'gsearch' 
+and utm_campaign = 'nonbrand'
+and created_at < '2012-05-10'
+group by week(created_at)
+)temp;
+
+-- Traffic source bid optimization
+select device_type,
+	   count(distinct ws.website_session_id) as sessions, 
+       count(order_id) as orders,
+	   count(order_id)/count(distinct ws.website_session_id)*100 as csr
+from website_sessions ws
+	left join orders ord
+		on ws.website_session_id = ord.website_session_id
+where ws.created_at < '2012-05-11'and utm_source = 'gsearch' and utm_campaign = 'nonbrand'
+group by device_type;
+
+
+-- Segment Trending
+
+select 
+-- week(created_at),
+min(date(created_at)),
+count(case when device_type = 'mobile' then website_session_id else null end) as 'mobile_ses',
+count(case when device_type = 'desktop' then website_session_id else null end) as 'desktop_ses'
+from website_sessions
+where utm_source = 'gsearch' and utm_campaign = 'nonbrand' and created_at between '2012-04-15' and '2012-06-09'
+group by week(created_at)
 
 
 
