a7114d0471ef8e135067a5478c82129415f0d525
Aman gupta <amang4996@gmail.com>
2022-06-03 13:41:37 +0530

    Update StrataScratch.sql

--- StrataScratch.sql
+++ StrataScratch.sql
@@ -39,3 +39,28 @@ left join amazon_transactions b
 on a.user_id = b.user_id
 where a.id>b.id
 and abs(datediff(a.created_at, b.created_at))<=7
+
+
+--Top 5 States With 5 Star Businesses
+select distinct state, count(business_id) as co from yelp_business
+where stars = 5
+group by state
+order by co desc, state asc
+limit 6
+
+
+
+--Workers With The Highest Salaries
+with temp as (
+    select w.worker_id, w.first_name, w.salary,t.worker_title,
+    dense_rank()oveR(order by salary desc) as dk
+    from worker w
+    inner join title t
+    on w.worker_id = t.worker_ref_id
+) 
+select worker_title from temp 
+where dk = 1 
+order by worker_title
+
+
+
