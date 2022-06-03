4f2538952b630130aec7c180e0181e6db986960b
Aman gupta <amang4996@gmail.com>
2022-06-03 13:39:12 +0530

    Update StrataScratch.sql

--- StrataScratch.sql
+++ StrataScratch.sql
@@ -11,3 +11,14 @@ select t.user, (sum(c1)/count(*)over())*100 as popularity_percent
 from temp t
 group by t.user 
 order by t.user
+
+
+--Classify Business Type
+select distinct business_name,
+case when lower(business_name) like '%school%' then 'school' 
+     when lower(business_name) like '%restaurant%' then 'restaurant'
+     when lower(business_name) like '%coffee%' or lower(business_name) like '%cafe%' or lower(business_name) like '%café%' then 'cafe'
+else 'other'
+end as business_type
+from sf_restaurant_health_violations
+
