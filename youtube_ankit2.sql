7a159b49168a2225052a1c9bc601d5c659b6794a
Aman gupta <amang4996@gmail.com>
2022-07-30 23:53:05 +0530

    Update youtube_ankit2.sql

--- youtube_ankit2.sql
+++ youtube_ankit2.sql
@@ -8,3 +8,21 @@ from events2 where gold in (
 	select bronze as name  from events2
 )
 order by gold
+
+
+--Leetcode-1412 Hard SQL Problem | Find the Quiet Students in All Exams
+
+with temp as (
+select s2.*, e2.exam_id, e2.score from students2 s2
+inner join exams2 e2
+on s2.student_id = e2.student_id
+), temp2 as(
+select *, max(score)over(partition by exam_id) as maxi,
+min(score)over(partition by exam_id) as mini
+from temp
+)
+select student_id, student_name from 
+temp2 where score != maxi and score != mini
+except
+select student_id, student_name from 
+temp2 where score = maxi or score = mini
