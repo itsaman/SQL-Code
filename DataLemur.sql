ae681d26e3118022335ca29a07a9b10e883c0d1f
Aman gupta <amang4996@gmail.com>
2022-08-28 12:20:45 +0530

    Update DataLemur.sql

--- DataLemur.sql
+++ DataLemur.sql
@@ -1,1 +1,9 @@
-
+--Cities With Completed Trades
+SELECT distinct u.city, count(1) as total_orders
+FROM trades t
+inner join users u
+on t.user_id = u.user_id
+where status = 'Completed'
+group by u.city
+order by total_orders desc
+limit 3
