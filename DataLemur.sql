2f635c9cbf7ae837eff0f0217f43b0800d211d7c
Aman gupta <amang4996@gmail.com>
2022-08-28 13:23:39 +0530

    Update DataLemur.sql

--- DataLemur.sql
+++ DataLemur.sql
@@ -20,3 +20,9 @@ SELECT
 sum(case when device_type ='laptop' then 1 else 0 end) as "laptop_views",
 sum(case when device_type = 'tablet' or device_type = 'phone' then 1 else 0 end) as "mobile_views"
 FROM viewership
+
+--Purchasing Activity by Product Type
+SELECT order_date,product_type,
+sum(quantity)over(partition by product_type order by order_date) as cum_purchased
+FROM total_trans
+order by order_date
