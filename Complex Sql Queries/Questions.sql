96f660ca7387f07416e3f735c41f10dcae8275ae
Aman gupta <amang4996@gmail.com>
2023-06-26 22:23:30 +0530

    Update Questions.sql

--- Complex Sql Queries/Questions.sql
+++ Complex Sql Queries/Questions.sql
@@ -19,3 +19,17 @@ select al.teamid,al.memberid, al.criteria1, al.criteria2,
 case when (criteria1 in ('y','Y') and criteria2 in ('y', 'Y')) and result>=2 then 'YES' else 'NO' end as output
 from Ameriprise_LLC al
 left join temp t on al.teamid = t.teamid;
+
+-- PayPal SQL Interview Problem (Level Hard) 
+with department as (
+select  department_id as department_id, avg(salary) as sal  from emp
+	group by department_id
+), all_data as (
+select dep.department_id as dep_id, sal, e.* from department dep, emp e
+where dep.department_id != e.department_id
+), final_res as (
+select *, avg(salary)over(partition by dep_id) as avg_sal
+from all_data
+)
+select distinct dep_id from final_res
+where sal<avg_sal
